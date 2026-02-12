47abc25eea77a44859dbf6ed41e6a5a054d5670b
J92 <ionutrobertbalasoiu@gmail.com>
2026-02-12 23:43:39 +0200

    lang switcher menu dropdown style class

--- resources/views/filament/hooks/lang-switcher.blade.php
+++ resources/views/filament/hooks/lang-switcher.blade.php
@@ -2,7 +2,7 @@
     <x-filament::dropdown maxHeight="250px" placement="left-start" teleport="true">
         <x-slot name="trigger">
             <div
-                class="flex cursor-pointer items-center justify-start gap-2 rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-800">
+                class="fi-dropdown-list-item fi-dropdown-list-item-color-gray fi-color-gray flex w-full items-center gap-2 whitespace-nowrap rounded-md p-2 text-sm outline-none transition-colors duration-75 hover:bg-gray-50 focus-visible:bg-gray-50 disabled:pointer-events-none disabled:opacity-70 dark:hover:bg-white/5 dark:focus-visible:bg-white/5">
                 <x-filament::icon icon="heroicon-c-chevron-left" class="mx-1 h-5 w-5 text-gray-500 dark:text-gray-400" />
                 {{ __('Language') }}
             </div>
